110a71f98f3717544f95b0a6610e7b9bd4a1ffff
benzco89 <ben.cohen1@gmail.com>
2024-08-29 10:21:26 +0300

    Fix build and deployment issues

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';  // או './styles/globals.css' אם זה הנתיב הנכון
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
